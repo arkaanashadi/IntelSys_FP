6ee567d8634cb82e45a8ac0ac2c4b6ecd88d6e78
arkaanashadi <arkaan.ashadi@gmail.com>
2019-10-07 18:33:48 +0700

    added fill function

--- main.py
+++ main.py
@@ -12,51 +12,54 @@ def generate_board():
     # Populate board
     game_board = [[0 for i in range(0, s_row)] for x in range(0, s_column)]
     return game_board
-# def check_full(column):
-#     print(board[-1][column])
 
-# def player():
-#     ch_column
 
-# print(board[0][1])
-# check_full(0)
+def fill(board, column, player):
 
-def fill(column):
-    pass
+    # last value in column
+    row = len(board[column]) - 1
 
-def check(board, column, row): # Checks if the column is full
+    # Determines where is the bottom of the row
+    for curr_row in range(0, row + 1):
+        if board[column][curr_row] != 0:
+            continue
+        else:
+            row = curr_row
+    board[column][row] = player
+
+def check(board, column):  # Checks if the column is full
     # print(board[column][row])
 
     # Checks if the top of the column is not 0, meaning a full column
-    if board[column][row] != 0:
+    if board[column][0] != 0:
         print("gaboleh")
         return False
     else:
         print("boleh")
         return True
 
+
 def player_turn(board):
     # User input for which row
     column = int(input("Select column:"))
+    if check(board, column):
+        fill(board, column, 1)
 
-    # Determines where is the bottom of the row
-    row = len(board[0]) - 1
-    for i in board[column]:
-        if board[column][i] != 0:
-            row = i
-            break
-        else:
-            break
-    check(board, column, row)
 
 def ai_turn(board):
-    True
+    # True
+    pass
+
 
 def main():
     board = generate_board()
     print(board)
-    player_turn(board)
-    print(board)
-    ai_turn(board)
-    print(board)
+    while True:
+
+        player_turn(board)
+        print(board)
+        ai_turn(board)
+        # print(board)
+
+
 main()
